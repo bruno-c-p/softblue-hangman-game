7d43c42090544d3f097ccc003a1bf18b30e138e3
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2022-09-01 09:14:37 -0300

    Implementando classe UI

---
+++ src/main/java/br/com/brunocp/hangman/core/exception/InvalidCharacterException.java
@@ -0,0 +1,8 @@
+package br.com.brunocp.hangman.core.exception;
+
+public class InvalidCharacterException extends Exception {
+
+    public InvalidCharacterException(String message) {
+        super(message);
+    }
+}
